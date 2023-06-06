9d90d30ab5235eb699ad9cb6f579d51ad472802b
Zalkary4 <zalkarbek.tilenbaev.kk@gmail.com>
2023-06-07 00:12:54 +0600

    update

--- piano_app/lib/home_page.dart
+++ piano_app/lib/home_page.dart
@@ -6,8 +6,50 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Piano'),
+      appBar: AppBar(title: const Text('Piano')),
+      body: Column(
+        children: [
+          Expanded(
+            child: Container(
+              color: Colors.red,
+            ),
+          ),
+          Expanded(
+            child: Container(
+              color: Colors.red,
+            ),
+          ),
+          Expanded(
+            child: Container(
+              color: Colors.orange,
+            ),
+          ),
+          Expanded(
+            child: Container(
+              color: Colors.yellow,
+            ),
+          ),
+          Expanded(
+            child: Container(
+              color: Colors.green,
+            ),
+          ),
+          Expanded(
+            child: Container(
+              color: Colors.blue,
+            ),
+          ),
+          Expanded(
+            child: Container(
+              color: Colors.indigo,
+            ),
+          ),
+          Expanded(
+            child: Container(
+              color: const Color.fromARGB(255, 72, 17, 222),
+            ),
+          ),
+        ],
       ),
     );
   }

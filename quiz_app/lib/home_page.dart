c9160ae378828f257f293af3b48d5b968dcfa2b3
Zalkary4 <zalkarbek.tilenbaev.kk@gmail.com>
2023-06-10 23:53:30 +0600

    update

--- quiz_app/lib/home_page.dart
+++ quiz_app/lib/home_page.dart
@@ -11,9 +11,49 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color(0xff202020),
       appBar: AppBar(
-        title: const Text('Quiz App'),
+        backgroundColor: Colors.white,
+        title: const Text(
+          'Quiz App',
+          style: TextStyle(color: Colors.black),
+        ),
         centerTitle: true,
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          const Text(
+            'Кыргызстанда 7 область бар',
+            textAlign: TextAlign.center,
+            style: TextStyle(color: Colors.white, fontSize: 30),
+          ),
+          QuizButton(),
+          QuizButton(),
+        ],
+      ),
+    );
+  }
+}
+
+class QuizButton extends StatelessWidget {
+  const QuizButton(this.isTrue, {super.key});
+  final bool isTrue;
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: double.infinity,
+      height: 40,
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+            backgroundColor: isTrue ? Colors.green : Colors.red),
+        onPressed: () {},
+        child: const Text(
+          'Туура',
+          style: TextStyle(
+            fontSize: 26,
+          ),
+        ),
       ),
     );
   }
--- quiz_app/test/widget_test.dart
+++ quiz_app/test/widget_test.dart
@@ -7,6 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:quiz_app/app.dart';
 
 import 'package:quiz_app/main.dart';
 

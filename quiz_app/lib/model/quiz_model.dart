c491d35ec6e2ecdec45a2dd4989f3a98abee2c79
Zalkary4 <zalkarbek.tilenbaev.kk@gmail.com>
2023-06-11 13:09:56 +0600

    update

--- quiz_app/lib/model/quiz_model.dart
+++ quiz_app/lib/model/quiz_model.dart
@@ -3,3 +3,10 @@ class Quiz {
   final String question;
   final bool answer;
 }
+
+const q1 = Quiz('Кыргызстанда 7 область барбы', true);
+const q2 = Quiz('Кыргызстандые борбору Бишкекпи?', true);
+const q3 = Quiz('Эн кичина материк бул Тундук Америкабы?', false);
+const q4 = Quiz('Ошко 3000 жыл болдубу?', true);
+const q5 = Quiz('Жер тоголокпу?', true);
+const q6 = Quiz('Ай жерден чонбу?', false);

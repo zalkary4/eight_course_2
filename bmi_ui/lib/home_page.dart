bb2d8b0cf695c973230400cc268ce8db1eaa6529
Zalkary4 <zalkarbek.tilenbaev.kk@gmail.com>
2023-06-12 22:24:02 +0600

    update

--- bmi_ui/lib/home_page.dart
+++ bmi_ui/lib/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:bmi_ui/components/calculate_button.dart';
 import 'package:bmi_ui/utils/app_colors.dart';
+import 'package:bmi_ui/utils/app_texts.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -16,7 +17,7 @@ class _HomePageState extends State<HomePage> {
       appBar: AppBar(
         backgroundColor: AppColor.backgroundColor,
         title: const Text(
-          'BMI calculator',
+          AppText.appBarTitle,
           style: TextStyle(
             fontSize: 24,
             fontWeight: FontWeight.w600,
@@ -26,11 +27,52 @@ class _HomePageState extends State<HomePage> {
       ),
       body: const Column(
         children: [
-          Expanded(child: Text('data')),
+          Expanded(
+            child: Row(
+              children: [
+                StatusCard(),
+              ],
+            ),
+          ),
           Expanded(child: Text('data')),
           Expanded(child: Text('data')),
           CalculateButton(),
         ],
+      ),
+    );
+  }
+}
+
+class StatusCard extends StatelessWidget {
+  const StatusCard({
+    super.key,
+    required this.icon,
+    required this.text,
+  });
+  final IconData icon;
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return const Expanded(
+      child: Card(
+        color: AppColor.cardColor,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(
+              Icons.male,
+              size: 40,
+            ),
+            Text(
+              AppText.male,
+              style: TextStyle(
+                color: AppColor.greyText,
+                fontSize: 24,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }

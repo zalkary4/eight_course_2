8d3c1fe522c94db8f1a5b3484222af6391e5e2c3
Zalkary4 <zalkarbek.tilenbaev.kk@gmail.com>
2023-06-12 22:03:55 +0600

    update

--- bmi_ui/lib/utils/app_colors.dart
+++ bmi_ui/lib/utils/app_colors.dart
@@ -5,5 +5,6 @@ class AppColor {
   static const Color buttonColor = Color(0xfff50d57);
   static const Color greyText = Color(0xff878892);
   static const Color whiteText = Color(0xffffffff);
-  static const Color whiteTrext = Color(0xffffffff);
+  static const Color cardColor = Color(0xff111327);
+  static const Color button2Color = Color(0xff4c4f5d);
 }

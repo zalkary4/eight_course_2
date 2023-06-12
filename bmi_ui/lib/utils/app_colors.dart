bf730286b348fbf4093709f8815fd793b77b6be9
Zalkary4 <zalkarbek.tilenbaev.kk@gmail.com>
2023-06-12 20:45:14 +0600

    update

--- bmi_ui/lib/utils/app_colors.dart
+++ bmi_ui/lib/utils/app_colors.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 class AppColor {
-  static Color backgroundColor = Color(0xff0a0f20);
+  static const Color backgroundColor = Color(0xff0a0f20);
   static const Color buttonColor = Color(0xfff50d57);
+  static const Color greyText = Color(0xff878892);
+  static const Color whiteText = Color(0xffffffff);
 }

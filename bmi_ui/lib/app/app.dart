464a35286e44e1a0f80cbd7f6701daec22f81c57
Zalkary4 <zalkarbek.tilenbaev.kk@gmail.com>
2023-06-12 22:44:19 +0600

    update

--- bmi_ui/lib/app/app.dart
+++ bmi_ui/lib/app/app.dart
@@ -1,4 +1,4 @@
-import 'package:bmi_ui/home_page.dart';
+import 'package:bmi_ui/views/home_view.dart';
 import 'package:flutter/material.dart';
 
 class MyApp extends StatelessWidget {
--- bmi_ui/lib/main.dart
+++ bmi_ui/lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:bmi_ui/app.dart';
+import 'package:bmi_ui/app/app.dart';
 import 'package:flutter/material.dart';
 
 void main() {

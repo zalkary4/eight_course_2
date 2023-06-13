73bde06eccb9f5fa7f81f20acb7d0567669b5762
Zalkary4 <zalkarbek.tilenbaev.kk@gmail.com>
2023-06-13 22:46:08 +0600

    update

--- bmi_app/lib/components/status_card.dart
+++ bmi_app/lib/components/status_card.dart
@@ -5,32 +5,100 @@ import 'package:flutter/material.dart';
 class StatusCard extends StatelessWidget {
   const StatusCard({
     super.key,
-    required this.icon,
-    required this.text,
+    required this.child,
   });
-  final IconData icon;
-  final String text;
+  final Widget child;
 
   @override
   Widget build(BuildContext context) {
     return Expanded(
       child: Card(
         color: AppColor.cardColor,
-        child: Column(
+        child: child,
+      ),
+    );
+  }
+}
+
+class MaleFemale extends StatelessWidget {
+  const MaleFemale({
+    super.key,
+    required this.icon,
+    required this.text,
+  });
+
+  final IconData icon;
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(
+          icon,
+          size: 80,
+        ),
+        const SizedBox(height: 10),
+        Text(
+          text,
+          style: AppTextStyle.greyTextStyle,
+        ),
+      ],
+    );
+  }
+}
+
+class WeightEight extends StatelessWidget {
+  const WeightEight({
+    super.key,
+    required this.text,
+    required this.value,
+  });
+
+  final String text;
+  final int value;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        const SizedBox(height: 10),
+        Text(
+          text,
+          style: AppTextStyle.greyTextStyle,
+        ),
+        Text(
+          '$value',
+          style: AppTextStyle.valueStyle,
+        ),
+        const Row(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Icon(
-              icon,
-              size: 80,
-            ),
-            const SizedBox(height: 10),
-            Text(
-              text,
-              style: AppTextStyle.greyTextStyle,
-            ),
+            RemoveAddButton(iconData: Icons.remove),
+            SizedBox(width: 20),
+            RemoveAddButton(iconData: Icons.add),
           ],
         ),
-      ),
+      ],
+    );
+  }
+}
+
+class RemoveAddButton extends StatelessWidget {
+  const RemoveAddButton({
+    super.key,
+    required this.iconData,
+  });
+  final IconData iconData;
+  @override
+  Widget build(BuildContext context) {
+    return FloatingActionButton(
+      backgroundColor: AppColor.button2Color,
+      foregroundColor: AppColor.whiteText,
+      onPressed: () {},
+      child: Icon(iconData),
     );
   }
 }

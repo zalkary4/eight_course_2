15b95365fbc1dfd47ca201a660b7c72ae1165082
Zalkary4 <zalkarbek.tilenbaev.kk@gmail.com>
2023-06-15 22:26:22 +0600

    update

--- game_test/lib/app/app.dart
+++ game_test/lib/app/app.dart
@@ -8,9 +8,9 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
+          // colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          // useMaterial3: true,
+          ),
       home: null,
     );
   }

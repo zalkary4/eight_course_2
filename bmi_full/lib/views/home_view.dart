5224ddbc4b689d38f770636d356a1389281fc4a4
Zalkary4 <zalkarbek.tilenbaev.kk@gmail.com>
2023-06-15 21:22:07 +0600

    update

--- bmi_full/lib/views/home_view.dart
+++ bmi_full/lib/views/home_view.dart
@@ -148,9 +148,9 @@ Future<void> showMyDialog(BuildContext context, String text) async {
   return showDialog<void>(
     context: context,
     builder: (BuildContext context) => AlertDialog(
-      // backgroundColor: AppColor.backgroundColor,
+      backgroundColor: AppColor.backgroundColor,
       title: const Center(child: Text(AppText.appBarTitle)),
-      content: Center(child: Text(text, style: AppTextStyle.resTextStyle)),
+      content: Text(text, style: AppTextStyle.resTextStyle),
       actions: [
         TextButton(
           onPressed: () => Navigator.pop(context),

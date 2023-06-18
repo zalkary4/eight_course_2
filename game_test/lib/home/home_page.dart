635cea3d92347e4360cb2cc1161ba8a37d118ccb
Zalkary4 <zalkarbek.tilenbaev.kk@gmail.com>
2023-06-18 20:26:41 +0600

    update

--- game_test/lib/home/home_page.dart
+++ game_test/lib/home/home_page.dart
@@ -39,10 +39,10 @@ class _MyHomePageState extends State<MyHomePage> {
           ],
         ),
       ),
-      // floatingActionButton: FloatingActionButton(
-      //   onPressed: _incrementCounter,
-      //   tooltip: 'Increment',
-      //   child: const Icon(Icons.add),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
       ),
     );
   }

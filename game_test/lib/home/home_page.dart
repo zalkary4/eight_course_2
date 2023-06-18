624d3ab226006467097755621c71ae8c6cb32a68
Zalkary4 <zalkarbek.tilenbaev.kk@gmail.com>
2023-06-18 20:28:21 +0600

    update

--- game_test/lib/home/home_page.dart
+++ game_test/lib/home/home_page.dart
@@ -13,9 +13,9 @@ class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
 
   void _incrementCounter() {
-    // setState(() {
-    //   _counter++;
-    // });
+    setState(() {
+      _counter++;
+    });
   }
 
   @override

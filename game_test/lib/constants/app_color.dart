0e034343d02c0876f24ffdaa3360e77368a77423
Zalkary4 <zalkarbek.tilenbaev.kk@gmail.com>
2023-06-19 17:05:39 +0600

    update

---
+++ game_test/lib/constants/app_color.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color bgColor = Color(0xfff0f0f0);
+  static const Color white = Color(0xffFFFFFF);
+  static const Color blue = Color(0xff0700ef);
+}

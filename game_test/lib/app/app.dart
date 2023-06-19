2298e95bec579054323263cb3a444d8ce90b09df
Zalkary4 <zalkarbek.tilenbaev.kk@gmail.com>
2023-06-19 16:50:32 +0600

    update

--- game_test/lib/app/app.dart
+++ game_test/lib/app/app.dart
@@ -6,7 +6,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter emo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,

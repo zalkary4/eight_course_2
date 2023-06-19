a868bf346f697a724e5a52245fe8206933b36291
Zalkary4 <zalkarbek.tilenbaev.kk@gmail.com>
2023-06-19 17:14:42 +0600

    update

--- game_test/lib/home/home_page.dart
+++ game_test/lib/home/home_page.dart
@@ -1,16 +1,15 @@
 import 'package:flutter/material.dart';
 
 class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
+  const MyHomePage({
+    super.key,
+  });
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(title),
       ),
       body: Center(),
     );

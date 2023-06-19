6bb77f84ea9db8f250b7c62643edc28539dbf74b
Zalkary4 <zalkarbek.tilenbaev.kk@gmail.com>
2023-06-19 17:06:35 +0600

    update

--- game_test/lib/constants/app_color.dart
+++ game_test/lib/constants/app_color.dart
@@ -4,4 +4,5 @@ class AppColors {
   static const Color bgColor = Color(0xfff0f0f0);
   static const Color white = Color(0xffFFFFFF);
   static const Color blue = Color(0xff0700ef);
+  static const Color red = Color(0xffD32f2f);
 }

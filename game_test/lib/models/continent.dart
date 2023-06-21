56a65793da69dfdd0fb1cea89b63a6c226e162af
Zalkary4 <zalkarbek.tilenbaev.kk@gmail.com>
2023-06-21 23:53:32 +0600

    update

---
+++ game_test/lib/models/continent.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Continent {
+  Continent({
+    required this.name,
+    required this.icon,
+    required this.color,
+  });
+  final String name;
+  final String icon;
+  final Color color;
+}

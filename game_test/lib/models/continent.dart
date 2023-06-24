5ece02c7338dce9b806a60bb0920f1b1c5cc728c
Zalkary4 <zalkarbek.tilenbaev.kk@gmail.com>
2023-06-24 19:37:45 +0600

    update

--- game_test/lib/models/continent.dart
+++ game_test/lib/models/continent.dart
@@ -15,5 +15,30 @@ class Continent {
 final africa = Continent(
   name: AppText.africa,
   icon: 'africa',
-  color: Color(0xfffeef34),
+  color: const Color(0xfffeef34),
+);
+final asia = Continent(
+  name: AppText.asia,
+  icon: 'asia',
+  color: Color(0xfff08180),
+);
+final europe = Continent(
+  name: AppText.europe,
+  icon: 'europe',
+  color: const Color(0xff81a2fd),
+);
+final nAmerica = Continent(
+  name: AppText.northAmerica,
+  icon: 'north_america',
+  color: const Color(0xfffeb000),
+);
+final sAmerica = Continent(
+  name: AppText.southAmerica,
+  icon: 'south_america',
+  color: const Color(0xffe5a9e4),
+);
+final australia = Continent(
+  name: AppText.australia,
+  icon: 'australia',
+  color: const Color(0xff6bf782),
 );

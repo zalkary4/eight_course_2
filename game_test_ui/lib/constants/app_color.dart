df99f2522f6688a2483a6be03ac2bba8601c2500
Zalkary4 <zalkarbek.tilenbaev.kk@gmail.com>
2023-06-26 22:22:13 +0600

    update

---
+++ game_test_ui/lib/constants/app_color.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color bgColor = Color(0xfff0f0f0);
+  static const Color white = Color(0xffFFFFFF);
+  static const Color blue = Color(0xff0700ef);
+  static const Color red = Color(0xffD32f2f);
+}
---
+++ game_test_ui/lib/models/continent.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:game_test/constants/app_text.dart';
+
+class Continent {
+  Continent({
+    required this.name,
+    required this.icon,
+    required this.color,
+  });
+  final String name;
+  final String icon;
+  final Color color;
+}
+
+final africa = Continent(
+  name: AppText.africa,
+  icon: 'africa',
+  color: const Color(0xfffeef34),
+);
+final asia = Continent(
+  name: AppText.asia,
+  icon: 'asia',
+  color: Color(0xfff08180),
+);
+final europe = Continent(
+  name: AppText.europe,
+  icon: 'europe',
+  color: const Color(0xff81a2fd),
+);
+final nAmerica = Continent(
+  name: AppText.northAmerica,
+  icon: 'north_america',
+  color: const Color(0xfffeb000),
+);
+final sAmerica = Continent(
+  name: AppText.southAmerica,
+  icon: 'south_america',
+  color: const Color(0xffe5a9e4),
+);
+final australia = Continent(
+  name: AppText.australia,
+  icon: 'australia',
+  color: const Color(0xff6bf782),
+);
+
+List<Continent> continents = [
+  europe,
+  asia,
+  nAmerica,
+  sAmerica,
+  africa,
+  australia
+];

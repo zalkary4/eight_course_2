a8dfb61ce732850d6005dd60f75d9909775b706f
Zalkary4 <zalkarbek.tilenbaev.kk@gmail.com>
2023-06-28 20:24:26 +0600

    update

---
+++ game_test_full/lib/models/suroo.dart
@@ -0,0 +1,15 @@
+class Suroo {
+  const Suroo(
+    this.text,
+    this.jooptor,
+    this.image,
+  );
+  final String text;
+  final List<Joop> jooptor;
+  final String image;
+}
+
+class Joop {
+  final bool isTrue;
+  final String text;
+}

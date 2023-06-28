1247b04282ec6327776156ce1a63dcb469b6689a
Zalkary4 <zalkarbek.tilenbaev.kk@gmail.com>
2023-06-28 08:15:01 +0600

    update

--- game_test_ui/lib/app/app.dart
+++ game_test_ui/lib/app/app.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:game_test/home/home_page.dart';
+import 'package:game_test/pages/home_page.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});

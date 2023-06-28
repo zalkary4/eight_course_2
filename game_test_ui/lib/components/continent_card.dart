03282415b1e69799e2b998428cdcdc873fbfb03b
Zalkary4 <zalkarbek.tilenbaev.kk@gmail.com>
2023-06-28 08:33:43 +0600

    update

--- game_test_ui/lib/components/continent_card.dart
+++ game_test_ui/lib/components/continent_card.dart
@@ -12,24 +12,29 @@ class ContinentCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      child: Column(
-        children: [
-          const SizedBox(height: 8),
-          Text(
-            item.name,
-            style: const TextStyle(fontSize: 22),
-          ),
-          Expanded(
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: SvgPicture.asset(
-                'assets/icons/continents/${item.icon}.svg',
-                color: item.color,
+    return InkWell(
+      onTap: () {},
+      child: Card(
+        child: Column(
+          children: [
+            const SizedBox(height: 8),
+            Text(
+              item.name,
+              style: const TextStyle(fontSize: 22),
+            ),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: InkWell(
+                  child: SvgPicture.asset(
+                    'assets/icons/continents/${item.icon}.svg',
+                    color: item.color,
+                  ),
+                ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }

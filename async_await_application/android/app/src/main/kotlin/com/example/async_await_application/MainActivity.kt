b9cfab7d25bd64c6dc67b54c5ba94fc48a58fb4b
Zalkary4 <zalkarbek.tilenbaev.kk@gmail.com>
2023-07-09 18:54:22 +0600

    update

---
+++ async_await_application/android/app/src/main/kotlin/com/example/async_await_application/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.async_await_application
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ async_await_application/lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:async_await_application/app.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}

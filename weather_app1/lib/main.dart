174d53ca204562b84a194cc5bc49ffeb44d821ed
Zalkary4 <zalkarbek.tilenbaev.kk@gmail.com>
2023-07-10 20:22:47 +0600

    update

--- weather_app1/lib/main.dart
+++ weather_app1/lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Dem',
+      title: 'Flutter Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,

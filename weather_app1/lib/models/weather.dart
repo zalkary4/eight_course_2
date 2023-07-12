e6752f33c823224894d793d32c7d09b2e4039440
Zalkary4 <zalkarbek.tilenbaev.kk@gmail.com>
2023-07-12 20:23:25 +0600

    update

--- weather_app1/lib/models/weather.dart
+++ weather_app1/lib/models/weather.dart
@@ -1,5 +1,10 @@
 class Weather {
-  Weather(this.id, this.main, this.description, this.icon);
+  Weather({
+    required this.id,
+    required this.main,
+    required this.description,
+    required this.icon,
+  });
   final int id;
   final String main;
   final String description;

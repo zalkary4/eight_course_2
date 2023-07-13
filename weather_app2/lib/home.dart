d255eed11af8783d502358f07f0338b38cf37526
Zalkary4 <zalkarbek.tilenbaev.kk@gmail.com>
2023-07-13 19:33:20 +0600

    update

---
+++ weather_app2/lib/home.dart
@@ -0,0 +1,66 @@
+import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
+import 'package:weather_app1/constants/api_const.dart';
+
+import 'models/weather.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Future<Weather?> fetchData() async {
+    final dio = Dio();
+    final res = await dio.get(ApiConts.adress);
+    if (res.statusCode == 200) {
+      final Weather weather = Weather(
+        id: res.data['weather'][0]['id'],
+        main: res.data['weather'][0]['main'],
+        description: res.data['weather'][0]['description'],
+        icon: res.data['weather'][0]['icon'],
+        city: res.data['name'],
+        temp: res.data['main']['temp'],
+        country: res.data['sys']['country'],
+      );
+      return weather;
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    fetchData();
+  }
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('App Bar'), centerTitle: true),
+      body: Center(
+        child: FutureBuilder(
+            future: fetchData(),
+            builder: (ctx, sn) {
+              if (sn.hasData) {
+                return Column(
+                  children: [
+                    Text(sn.data!.id.toString()),
+                    Text(sn.data!.description),
+                    Text(sn.data!.main),
+                    Text(sn.data!.icon),
+                    Text(sn.data!.city),
+                    Text(sn.data!.country),
+                    Text(sn.data!.temp.toString()),
+                  ],
+                );
+              } else if (sn.hasError) {
+                return Text(sn.error.toString());
+              } else {
+                return CircularProgressIndicator();
+              }
+            }),
+      ),
+    );
+  }
+}

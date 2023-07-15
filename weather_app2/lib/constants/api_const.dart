356c8f6337c3e8dbdbec62f509a170d500e3738d
Zalkary4 <zalkarbek.tilenbaev.kk@gmail.com>
2023-07-15 19:16:05 +0600

    update

--- weather_app2/lib/constants/api_const.dart
+++ weather_app2/lib/constants/api_const.dart
@@ -1,4 +1,7 @@
 class ApiConts {
   static const adress =
       'https://api.openweathermap.org/data/2.5/weather?q=bishkek,&appid=41aa18abb8974c0ea27098038f6feb1b';
+  static String getIcon(String iconCode, int size) {
+    return 'http://openweathermap.org/img/wn/$iconCode@${size}x.png';
+  }
 }

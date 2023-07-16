c03a6eeba83cdf32c79f13a6098951b3d9cfd37b
Zalkary4 <zalkarbek.tilenbaev.kk@gmail.com>
2023-07-16 17:56:59 +0600

    update

--- weather_app2/lib/constants/app_text_styles.dart
+++ weather_app2/lib/constants/app_text_styles.dart
@@ -4,4 +4,6 @@ import 'package:weather_app1/constants/app_colors.dart';
 class AppTextStyle {
   static const appBar = TextStyle(color: AppColors.black, fontSize: 28);
   static const body1 = TextStyle(color: AppColors.white, fontSize: 96);
+  static const body2 = TextStyle(
+      color: AppColors.white, fontSize: 96, fontWeight: FontWeight.w600);
 }

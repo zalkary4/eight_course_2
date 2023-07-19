033a1dde78045494ad628d49c12ae4b798285c5f
Zalkary4 <zalkarbek.tilenbaev.kk@gmail.com>
2023-07-19 20:15:45 +0600

    update

--- weather_app3/test/widget_test.dart
+++ weather_app3/test/widget_test.dart
@@ -7,8 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:weather_app3/main.dart';
+import 'package:weather_app1/app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {

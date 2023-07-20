efa279387cb3f7b769e187d743fa274b843d5fe8
Zalkary4 <zalkarbek.tilenbaev.kk@gmail.com>
2023-07-20 23:17:53 +0600

    update

--- weather_app3/lib/constants/app_colors.dart
+++ weather_app3/lib/constants/app_colors.dart
@@ -3,4 +3,5 @@ import 'package:flutter/material.dart';
 class AppColors {
   static const Color white = Color(0xffffffff);
   static const Color black = Color(0xff000000);
+  static const Color red = Color.fromARGB(255, 250, 0, 0);
 }
